883d0199d0f78d8761d6cf27849c462eeeccbfc0
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-11 13:44:49 +0530

    DSA

---
+++ largest_element.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class largest_element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+
+        //int nums[] = new int[size];
+        int arr[] = new int[size];
+
+        System.out.println("Enter the element: ");
+        for(int i = 0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        //by using max variable
+        int max = arr[0];
+
+        for(int j=0;j<size;j++){
+            if(arr[j]>max){
+                max = arr[j];
+            }
+        }
+        System.out.println("Largest Element: ");
+        System.out.println(max+"");
+
+        sc.close();
+    }
+}
