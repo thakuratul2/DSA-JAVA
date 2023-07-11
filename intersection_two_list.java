c7a54f88795cb314ba39ddd807bbe25ef735bc2f
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-11 20:31:53 +0530

    DSA

---
+++ intersection_two_list.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class intersection_two_list {
+    public class ListNode {
+
+        public ListNode next;
+    
+    }
+    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //length of list A
+
+        int lenA = 0;
+        ListNode tempA = headA;
+
+        while(tempA!=null){
+            lenA++;
+            tempA = tempA.next;
+        }
+         //length of list B
+
+        int lenB = 0;
+        ListNode tempB = headB;
+
+        while(tempB!=null){
+            lenB++;
+            tempB = tempB.next;
+        }
+
+        //differnce
+        int diff = Math.abs(lenA - lenB);
+
+        //iterate the larger node
+
+        tempA = headA;
+        tempB= headB;
+
+        if(lenA > lenB){
+            while(diff-- > 0)
+            tempA = tempA.next;
+        }else{
+            while(diff-- > 0)
+            tempB = tempB.next;
+        }
+
+        //check the equality
+        while(tempA !=tempB){
+            tempA = tempA.next;
+            tempB = tempB.next;
+
+            if(tempA == null || tempB == null)
+            return null;
+        }
+        return tempA;
+    }
+}
