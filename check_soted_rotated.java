59f9b4f405dbc2e44b1acbac24e3f5babb97a2e5
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-13 20:16:53 +0530

    DSA

---
+++ check_soted_rotated.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class check_soted_rotated {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+
+        int nums[] = new int[size];
+
+        System.out.println("Enter the element: ");
+        for(int i = 0;i<size;i++){
+            nums[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for(int j = 0;j<size;j++){
+            if(nums[j]>nums[(j+1)%size]){
+                count++;
+            }
+            if(count>1){
+                System.out.println("False");
+            }
+        }
+        System.out.println("True");
+        sc.close();
+    }
+}
