40b90b8f12db5715b322db3bc4e3a539d85c6cb2
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-15 23:04:30 +0530

    DSA

---
+++ Stack/n_ary_preoder.java
@@ -0,0 +1,62 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class n_ary_preorder {
+    class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+    public void preorder(Node root, List<Integer> res){
+
+        if(root == null){
+            return;
+        }
+
+        res.add(root.val);
+
+        //put the node in the list from the tree
+        for(int i =0;i<root.children.size();i++){
+            preorder(root.children.get(i),res);
+        }
+    }
+    public List<Integer> preorder(Node root) {
+        
+        List<Integer> ans = new ArrayList<Integer>();
+
+        preorder(root,ans);
+
+        return ans;
+    }
+}
