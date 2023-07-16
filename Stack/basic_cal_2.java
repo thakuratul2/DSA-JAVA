1421380136be00910bcdd19b7ec40ab64b5e8561
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-16 20:59:39 +0530

    DSA

---
+++ Stack/basic_cal_2.java
@@ -0,0 +1,45 @@
+package Stack;
+
+import java.util.Stack;
+
+public class basic_cal_2 {
+    public int calculate(String s) {
+        
+        int curr = 0;
+        char op = '+';
+
+        Stack<Integer> st = new Stack<>();
+        char[] ch= s.toCharArray();
+
+        for(int i =0;i<ch.length;i++){
+            if(Character.isDigit(ch[i])){
+                curr = curr*10  + ch[i] - '0';
+            }
+
+            if(!Character.isDigit(ch[i]) && ch[i] != ' ' || i == ch.length-1){
+
+                if(op == '+'){
+                    st.push(curr);
+                }else if(op == '-'){
+                    st.push(-curr);
+                }else if(op == '*'){
+                    st.push(st.pop() * curr);
+                }
+                else if(op == '/'){
+                    st.push(st.pop() / curr);
+                }
+
+                op = ch[i];
+                curr = 0;
+            }
+
+        }
+
+        int sum = 0;
+        while(!st.isEmpty()){
+            sum +=st.pop();
+        }
+
+        return sum;
+    }
+}
