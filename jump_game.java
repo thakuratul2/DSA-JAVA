596833de6b73a9a750757f5c762cf413c7ea48f5
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-11 20:11:25 +0530

    DSA

--- jump_game.java
+++ jump_game.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class jump_game {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+
+        int[] nums = new int[size];
+
+        System.out.println("Enter the element: ");
+        for(int i = 0;i<size;i++){
+            nums[i] = sc.nextInt();
+        }
+
+        //track the index 
+        int reach = 0;
+
+        for(int j = 0; j<size;j++){
+
+            if(j > reach){
+                
+                System.out.println("Not Track");
+                
+            }
+            reach = Math.max(reach, j + nums[j]);
+        }
+        System.out.println("Jump Game Done");
+        sc.close();
     }
 }
