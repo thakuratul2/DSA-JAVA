8c817877244fad19f60eacb4fc393a6c73fd50d1
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-01 19:50:14 +0530

    DSA

--- delete_node_bst.java
+++ delete_node_bst.java
@@ -1,3 +1,45 @@
 public class delete_node_bst {
-    
+
+    class TreeNode{
+        int val;
+        int left;
+        int right;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return null;
+        }
+
+        if(key < root.val){
+            root.left = deleteNode(root.left, key);
+        }else if(key > root.val){
+            root.right = deleteNode(root.right, key);
+        }else{
+
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            root.val = minNode(root.right);
+
+            root.right = deleteNode(root.right, root.val);
+        }
+
+        return root;
+    }
+
+    public int minNode(TreeNode root){
+
+        int min = root.val;
+
+        while(root.left != null){
+
+            min = root.left.val;
+            root = root.left;
+        }
+
+        return min;
+    }
 }
