d35e8add6b62d7e3e1764b6e45c9577e85c2a079
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-01 19:25:51 +0530

    DSA

---
+++ delete_node_bst.java
@@ -0,0 +1,3 @@
+public class delete_node_bst {
+    
+}
--- insert_into_bst.java
+++ insert_into_bst.java
@@ -1,5 +1,26 @@
 public class insert_into_bst {
-    public static void main(String[] args) {
+
+    class TreeNode{
+
+        public int val;
+        public int left;
+        public int right;
         
     }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            //it return the value when the root is null
+            root = new TreeNode();//TreeNode(val)
+            return root;
+        } 
+      
+ 
+        if(val < root.val){
+            root.left = insertIntoBST(root.left,val);
+        }else{
+            root.right = insertIntoBST(root.right,val);
+        }
+ 
+        return root;
+     }
 }
