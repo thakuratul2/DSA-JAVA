1b6173e6cd825063dcaf377b5c2c3b88bd1cf8f8
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-02 11:14:49 +0530

    DSA

---
+++ min_add_to_make_pare.java
@@ -0,0 +1,23 @@
+public class min_add_to_make_pare {
+    public int minAddToMakeValid(String s) {
+        
+        int left = 0;
+        int right = 0;
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(s.charAt(i) == '('){
+                left++;
+            }else{
+
+                if(left > 0){
+                    left--;
+                }else{
+                    right++;
+                }
+            }
+
+        }
+        return left + right;
+    }
+}
