f8694e7ca79e1b64e1268cb6241cb4cf4ced99e6
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-03 19:53:34 +0530

    DSA

---
+++ isomorphicstring.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class isomorphicstring {
+
+    //by using hashmap
+   public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> hm = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(hm.containsKey(s.charAt(i))){
+
+                if(!hm.get(s.charAt(i)).equals(t.charAt(i)))
+                    return false;
+                
+            }else{
+                if(hm.containsValue(t.charAt(i)))
+                {
+return false;
+                }else{
+                    hm.put(s.charAt(i), t.charAt(i));
+                }
+                
+            }
+        }
+        return true;
+
+    }
+}
