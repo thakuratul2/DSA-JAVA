f07ad4ace992e25fd13dfeb65beb4ab059f55c26
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-09 18:01:18 +0530

    DSA

--- shortest_palidrome.java
+++ shortest_palidrome.java
@@ -7,6 +7,7 @@ for (int i = s.length() - 1; i >= 0; i--) {
 }
 if (j == s.length()) { return s; }
 String suffix = s.substring(j);
+
 return new StringBuffer(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
 }
 }
