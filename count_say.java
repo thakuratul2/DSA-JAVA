5893d952285db3ab33cc2912d88b0937da0db9b0
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-16 20:14:46 +0530

    DSA

--- count_say.java
+++ count_say.java
@@ -15,6 +15,7 @@ public class count_say {
             char c=s.charAt(0);
             
             int cnt=1;
+            
             for(int i=1;i<s.length();i++)
             {
                 if(s.charAt(i)==c)
