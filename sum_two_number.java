dbda14538fbe77c551f30009bf6ba142550e8f81
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-26 21:17:30 +0530

    DSA

---
+++ sum_two_number.java
@@ -0,0 +1,14 @@
+public class sum_two_number {
+    
+        public int getSum(int a, int b) {
+            if(a==0 || b==0) return a!=0?a:b;
+            int c=b,s=0;
+            while(c!=0){
+                s=a^b;
+                c=(a&b)<<1;
+                a=s;
+                b=c;
+            }
+            return s;
+        }
+}
