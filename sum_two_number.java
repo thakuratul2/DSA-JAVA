6607077b93b2e8fa687279b8e5a3826c24849062
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-26 21:18:16 +0530

    DSA

--- sum_two_number.java
+++ sum_two_number.java
@@ -2,6 +2,7 @@ public class sum_two_number {
     
         public int getSum(int a, int b) {
             if(a==0 || b==0) return a!=0?a:b;
+            //by not using + and - only use bit manipulation
             int c=b,s=0;
             while(c!=0){
                 s=a^b;
